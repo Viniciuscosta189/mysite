422d52be766c96d6fa6e0cb66b3c8834b796db1e
Viniciuscosta189 <seu-email-do-github@example.com>
2025-09-03 23:52:33 -0300

    Adiciona modelos Curso e Aluno no app blog e registra no admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Curso, Aluno
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "carga_horaria")
+    search_fields = ("nome",)
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email", "curso", "matriculado_em")
+    search_fields = ("nome", "email")
+    list_filter = ("curso",)
+
 
 # Register your models here.
